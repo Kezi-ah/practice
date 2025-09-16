794684028207afee9255e0836285a5554253c833
Kezi-ah <oluwanifemibabalola05@gmail.com>
2025-09-16 11:31:57 +0100

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,34 @@
+let grade = "F";
+
+switch (grade) {
+  case "A":
+    console.log("Excellent!");
+    break;
+  case "B":
+    console.log("Good job");
+    break;
+  case "C":
+    console.log("Fair, but you can improve");
+    break;
+  case "D":
+    console.log("Needs improvement");
+    break;
+  case "F":
+    console.log("Fail");
+    break;
+  default:
+    console.log("Not a valid grade");
+}
+
+
+const food = ["Rice","Beans","Spagetti","Bread","Fish"]
+console.log(food[2])
+food.push("Plantain")
+console.log (food.length)
+
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Nifemi");
+
